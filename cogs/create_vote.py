c9964729d3b9634670e9444d6d5dfce896b2529a
Nir <nirdafnai3@gmail.com>
2020-10-03 18:27:48 +0300

    Add voting system

--- cogs/create_vote.py
+++ cogs/create_vote.py
@@ -1,64 +1,64 @@
-import time
+from datetime import datetime
+import asyncio
 
-from discord import Embed, Colour
+from discord import Embed, RawReactionActionEvent, Message
 from discord.ext import commands
-from concurrent.futures._base import TimeoutError as DiscordTimeoutError
 
 AGREE = "✅"
 DISAGREE = "❎"
 
 
 class CreateVoteCog(commands.Cog):
-    def __init__(self, bot):
+    votes = []
+
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.context = None
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"[+] CreateVote command ready.")
+
+    async def create_vote_message(self, vote_question, vote_seconds):
+        vote_message = Embed(title="Poll",
+                             description=vote_question,
+                             colour=self.context.author.colour,
+                             timestamp=datetime.utcnow())
+
+        fields = [("Options", "\n".join((f"{AGREE} - Yes", f"{DISAGREE} - No")), False),
+                  ("Instructions", "React to cast a vote!", False)]
+        for name, value, inline in fields:
+            vote_message.add_field(name=name, value=value, inline=inline)
+
+        return vote_message
+
+    async def send_vote_message(self, vote_message):
+        message_sent = await self.context.send(embed=vote_message)
+        await message_sent.add_reaction(AGREE)
+        await message_sent.add_reaction(DISAGREE)
+        return message_sent
 
     @commands.command(name="create_vote", aliases=("cv", "createvote"))
-    async def create_vote(self, context: commands.Context, poll_time: int, *, question: str):
-        poll = Embed(title="Poll", description=question)
-        sent_msg = await context.channel.send(embed=poll)
-        await sent_msg.add_reaction(AGREE)
-        await sent_msg.add_reaction(DISAGREE)
-        current_time = time.time()
-        agree_count = 0
-        disagree_count = 0
-        while time.time() - current_time < poll_time:
-            try:
-                reaction, _ = await self.bot.wait_for('reaction_add',
-                                                      check=lambda user_reaction, user:
-                                                      user_reaction.message.id == sent_msg.id
-                                                      and user_reaction.me
-                                                      and (user_reaction.emoji == AGREE
-                                                           or user_reaction.emoji == DISAGREE), timeout=0.1)
-            except DiscordTimeoutError:
-                pass
-            else:
-                if reaction.emoji == AGREE:
-                    agree_count += 1
-                    print("Someone agreed")
-                elif reaction.emoji == DISAGREE:
-                    disagree_count += 1
-            try:
-                reaction, _ = await self.bot.wait_for('reaction_remove',
-                                                      check=lambda user_reaction, user:
-                                                      user_reaction.message.id == sent_msg.id
-                                                      and user.id != self.bot.user.id
-                                                      and (user_reaction.emoji == AGREE
-                                                           or user_reaction.emoji == DISAGREE), timeout=0.1)
+    async def create_vote(self, context: commands.Context, vote_seconds: int, *, vote_question: str):
+        self.context = context
 
-            except DiscordTimeoutError:
-                pass
-            else:
-                if reaction.emoji == AGREE:
-                    agree_count -= 1
-                    print("Someone agreed")
-                elif reaction.emoji == DISAGREE:
-                    disagree_count -= 1
-                    print("Someone disagreed")
+        vote_message = await self.create_vote_message(vote_question, vote_seconds)
+        message_sent = await self.send_vote_message(vote_message)
 
-        results = Embed(title="Poll Results", description=f"Results for the poll: {question}")
-        results.add_field(name="Results:", value=f"{AGREE}: {str(agree_count)}  {DISAGREE}: {str(disagree_count)}")
-        await context.channel.send(embed=results)
-        await sent_msg.delete()
+        CreateVoteCog.votes.append(message_sent)
+        await asyncio.sleep(vote_seconds)
+        await self.end_poll(context, message_sent, vote_question)
+
+    async def end_poll(self, context: commands.Context, vote_message: Message, vote_question: str):
+        live_message = await vote_message.channel.fetch_message(vote_message.id)
+
+        most_voted = max(live_message.reactions, key=lambda reactions: reactions.count)
+        least_voted = min(live_message.reactions, key=lambda reactions: reactions.count)
+        await context.send(f"Question: {vote_question}\n\n{most_voted.emoji} - {most_voted.count - 1}\n\n"
+                           f"{least_voted.emoji} - {least_voted.count - 1}")
+        CreateVoteCog.votes.remove(vote_message)
+        await live_message.delete()
+
 
 
 def setup(bot):
--- cogs/events.py
+++ cogs/events.py
@@ -1,8 +1,9 @@
 import config
 
-from discord import Member
-from discord import VoiceState
+from discord import Member, VoiceState, RawReactionActionEvent
 from discord.ext import commands
+
+from cogs.create_vote import CreateVoteCog
 
 
 class Events(commands.Cog):
@@ -16,6 +17,20 @@ class Events(commands.Cog):
                 if after.channel.id in config.VOICE_CHANNEL_SUMMON_IDS:
                     for text_channel_id in config.TEXT_CHANNEL_ALERT_IDS:
                         await self.bot.get_channel(text_channel_id).send(f"{member.display_name} is summoning @everyone")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: RawReactionActionEvent):
+        if payload.member.bot:
+            return
+        for vote_message in CreateVoteCog.votes:
+            if payload.message_id == vote_message.id:
+                live_message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+                for reaction in live_message.reactions:
+                    if payload.member in await reaction.users().flatten() and reaction.emoji != payload.emoji.name:
+                        await live_message.remove_reaction(reaction.emoji, payload.member)
+                break
+
+
 
 
 def setup(bot: commands.Bot):
