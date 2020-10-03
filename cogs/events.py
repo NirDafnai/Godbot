5f04a85384a3576e8374ef27a866a3a5780bf3e9
idoprog <ido.gamer313@gmail.com>
2020-10-03 18:21:03 +0300

    Fix typo

--- cogs/events.py
+++ cogs/events.py
@@ -13,8 +13,8 @@ class Events(commands.Cog):
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
         if before.channel != after.channel and after.channel is not None:
             if len(after.channel.members) == 1:
-                if after.channel.id in config.VOICE_CHANNEL_SUMMON:
-                    for text_channel_id in config.TEXT_CHANNEL_ALERT:
+                if after.channel.id in config.VOICE_CHANNEL_SUMMON_IDS:
+                    for text_channel_id in config.TEXT_CHANNEL_ALERT_IDS:
                         await self.bot.get_channel(text_channel_id).send(f"{member.display_name} is summoning @everyone")
 
 
