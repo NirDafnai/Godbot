e172c4107f6eafd4286226e447720237ae78a18c
idoprog <ido.gamer313@gmail.com>
2020-10-03 18:00:43 +0300

    Fix when now only the first user joins it summons

--- cogs/events.py
+++ cogs/events.py
@@ -12,9 +12,10 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
         if before.channel != after.channel and after.channel is not None:
-            if after.channel.id in config.VOICE_CHANNEL_SUMMON:
-                for text_channel_id in config.TEXT_CHANNEL_ALERT:
-                    await self.bot.get_channel(text_channel_id).send(f"{member.display_name} is summoning @everyone")
+            if len(after.channel.members) == 1:
+                if after.channel.id in config.VOICE_CHANNEL_SUMMON:
+                    for text_channel_id in config.TEXT_CHANNEL_ALERT:
+                        await self.bot.get_channel(text_channel_id).send(f"{member.display_name} is summoning @everyone")
 
 
 def setup(bot: commands.Bot):
