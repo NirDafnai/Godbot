f525c1c59d85884b8b89ce9e39596ed1981ab5f1
idoprog <ido.gamer313@gmail.com>
2020-10-03 17:55:21 +0300

    Add summon for when person enters channel

--- cogs/events.py
+++ cogs/events.py
@@ -1,9 +1,20 @@
+import config
+
+from discord import Member
+from discord import VoiceState
 from discord.ext import commands
 
 
 class Events(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
+        if before.channel != after.channel and after.channel is not None:
+            if after.channel.id in config.VOICE_CHANNEL_SUMMON:
+                for text_channel_id in config.TEXT_CHANNEL_ALERT:
+                    await self.bot.get_channel(text_channel_id).send(f"{member.display_name} is summoning @everyone")
 
 
 def setup(bot: commands.Bot):
